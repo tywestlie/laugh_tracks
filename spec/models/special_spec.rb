9a54be07e60d96e0ad5ef1a65028b0ff6a1eaa5a
Tyler Westlie <tyler.westlie@gmail.com>
2018-05-04 10:58:56 -0600

    final commit

--- spec/models/special_spec.rb
+++ spec/models/special_spec.rb
@@ -3,7 +3,6 @@ RSpec.describe Comedian do
     describe 'required fields' do
       it 'should be invalid if missing a special name' do
         mitch = Comedian.create(name: "Mitch Hedberg", age: 48)
-        require 'pry'; binding.pry
         mitch_special = Special.create(comedian_id: mitch.id)
 
         expect(mitch_special).to_not be_valid
